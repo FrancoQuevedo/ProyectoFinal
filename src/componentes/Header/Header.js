aa78470e3dd474239f6bf6e9503d6521aad88ed0
Franco Quevedo <franco.03_93@live.com.ar>
2022-11-25 09:55:21 -0300

    proyecto-Final-Utn

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -2,23 +2,18 @@ import React from "react";
 import Intro from "../Intro/Intro";
 import "./Header.css";
 /* import CustomButton from "../Button/customButton"; */
-
+import  Carousell  from '../Bienvenida/CarouselBootstrap';
 
 const Home = () => {
 
     return(
     <header>
-        <Intro titulo= " Cosme Fulanit" />        
-                       <h2>    Los Cerros Boutique Hotel   </h2>
-                       <p> Los Cerros es el hotel más emblemático del Chaltén. Su ubicación estratégica en la zona más elevada del pueblo, permite obtener vistas únicas del Valle del Río de las Vueltas. A su vez, se encuentra a pocos metros de los puntos de inicio de los principales senderos y recorridos.
-Las habitaciones y áreas públicas son espaciosas y están decoradas de manera clásica, en sintonía con el increíble entorno natural y sus impactantes paisajes.
-En cuanto al servicio, Los Cerros se destaca por su atención personalizada y un ambiente elegante pero relajado.
+        <Carousell/>
+        <div className="conteinerHeader">
+        <Intro/>        
 
-Cada vez son más los que eligen Los Cerros como destino vacacional. Verlos volver en cada temporada nos llena de satisfacción.
-
-¡Muchas gracias por elegirnos! </p>
     
-
+</div>
     </header>
 );
 
--- src/componentes/Intro/Intro.js
+++ src/componentes/Intro/Intro.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Intro.css';
 /* import Portada from './portada.jpg'; */
 import CustomButton from '../Button/CustomButton';
-import  Carousell  from '../Bienvenida/CarouselBootstrap';
+
 
 
 
@@ -13,13 +13,19 @@ const Intro = ({ titulo, backgroundcolor}) => {
      
      <div className='conteinerIntro'>
 
-     <Carousell />
+    
     {/* <img  src={Portada} className="icon" alt="Foto portada "/> */}
    
    
-{/*     <h1> {titulo} </h1> */}
+{/*    <h1> {titulo} </h1> */}
 
+   <h1> ' Los Andes Boutique Hotel '</h1>
+   <div className='textoConteiner'>
+         <p> Los Andes es el hotel más emblemático del Chaltén. Su ubicación estratégica en la zona más elevada del pueblo, permite obtener vistas únicas del Valle del Río de las Vueltas. A su vez, se encuentra a pocos metros de los puntos de inicio de los principales senderos y recorridos.
+Las habitaciones y áreas públicas son espaciosas y están decoradas de manera clásica, en sintonía con el increíble entorno natural y sus impactantes paisajes.
+En cuanto al servicio, Los Cerros se destaca por su atención personalizada y un ambiente elegante pero relajado.
 
+Cada vez son más los que eligen Los Cerros como destino vacacional. Verlos volver en cada temporada nos llena de satisfacción.¡Muchas gracias por elegirnos! </p></div>
 
     </div>
 )
